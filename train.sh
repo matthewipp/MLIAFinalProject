7a856f83858c68b41f1a006be0851d63c409a9b3
Anubhav Acharya <anubhavacharya@icloud.com>
2022-12-08 18:18:58 -0500

    Training slurm script

---
+++ train.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#SBATCH -A mlia
+#SBATCH -p gpu
+#SBATCH --gres=gpu:v100:1
+#SBATCH -c 1
+#SBATCH -t 00:01:00
+#SBATCH -o output.txt
+
+# Any commands after this will be submitted to queue using the 'sbatch' command
+# Queues can be listed using the 'squeue --me' command
+
+python TransUnet/train.py --root_path [training data path] --dataset Oxide --list_dir (list file of files to use) --num_classes 2 --deterministic 0 --img_size 64 --seed 69420 --vit_patches_size 32
