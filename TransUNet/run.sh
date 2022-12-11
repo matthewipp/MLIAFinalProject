87b8915f834c414d7fad308ba74b3a48a3d9749f
acharya8848 <anubhavacharya@icloud.com>
2022-12-10 21:27:38 -0500

    SLURM script

--- TransUNet/run.sh
+++ TransUNet/run.sh
@@ -13,10 +13,10 @@ module load pytorch/1.12.0
 cp ../lists/lists_Synapse/train.augmented.txt ../lists/lists_Synapse/train.txt
 
 # Train the model
-singularity run --nv /share/resources/containers/singularity/pytorch-1.12.0.sif train.py --root_path ../Data/augmented --list_dir ../lists/lists_Synapse --num_classes 2 --deterministic 0 --img_size 64 --seed 69420 --vit_patches_size 16 --n_gpu 2 --max_epochs 1000
+singularity run --nv $CONTAINERDIR/pytorch-1.12.0.sif train.py --root_path ../Data/augmented --list_dir ../lists/lists_Synapse --num_classes 2 --deterministic 0 --img_size 64 --seed 69420 --vit_patches_size 16 --n_gpu 2 --max_epochs 1000
 
 # Setup the list for the test data
 cp ../lists/lists_Synapse/test.augmented.txt ../lists/lists_Synapse/train.txt
 
 # Test the model
-singularity run --nv /share/resources/containers/singularity/pytorch-1.12.0.sif test.py --volume_path ../Data/augmented --list_dir ../lists/lists_Synapse --num_classes 2 --deterministic 0 --img_size 64 --seed 69420 --vit_patches_size 16 --max_epochs 150 --n_gpu 2
+singularity run --nv $CONTAINERDIR/pytorch-1.12.0.sif test.py --volume_path ../Data/augmented --list_dir ../lists/lists_Synapse --num_classes 2 --deterministic 0 --img_size 64 --seed 69420 --vit_patches_size 16 --max_epochs 150 --n_gpu 2
